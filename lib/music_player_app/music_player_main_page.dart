7fc3cbfd6b89747aa20114d95b6139485d297c75
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-02-21 19:35:21 +0630

    music player second commit

--- lib/music_player_app/music_player_main_page.dart
+++ lib/music_player_app/music_player_main_page.dart
@@ -66,10 +66,129 @@ class _MainPageState extends State<MainPage> {
             left: 48,
             child: Container(
               height: MediaQuery.of(context).size.height / 2.7,
+              padding: EdgeInsets.only(left: 24, top: 24),
               decoration: BoxDecoration(
                   color: Color(0xff6e00db),
                   borderRadius:
                       BorderRadius.only(topLeft: Radius.circular(42))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Recent Songs',
+                    style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Container(
+                    height: 205,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 16, left: 0),
+                          width: MediaQuery.of(context).size.width / 1.8,
+                          decoration: BoxDecoration(
+                              //color: Colors.yellow
+                              ),
+                          child: ListView.builder(
+                            padding: EdgeInsets.zero,
+                            itemBuilder: (BuildContext context, int index) =>
+                                Container(
+                              height: 84,
+                              margin: EdgeInsets.only(bottom: 16),
+                              decoration: BoxDecoration(
+                                  //color: Colors.purple
+                                  ),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 32,
+                                    backgroundImage: NetworkImage(
+                                        'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                                  ),
+                                  SizedBox(
+                                    width: 9,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Secrets',
+                                        style: GoogleFonts.montserrat(
+                                            color: Colors.white, fontSize: 18),
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Text(
+                                        'Not a Hobby 2020',
+                                        style: GoogleFonts.montserrat(
+                                            color: Colors.grey.shade200),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 16, left: 24),
+                          width: MediaQuery.of(context).size.width / 1.8,
+                          decoration: BoxDecoration(
+                              //color: Colors.yellow
+                              ),
+                          child: ListView.builder(
+                              padding: EdgeInsets.zero,
+                              itemBuilder: (BuildContext context, int index) =>
+                                  Container(
+                                    height: 84,
+                                    margin: EdgeInsets.only(bottom: 16),
+                                    child: Row(
+                                      children: [
+                                        CircleAvatar(
+                                          radius: 32,
+                                          backgroundImage: NetworkImage(
+                                              'https://images.unsplash.com/photo-1645390747975-fb637273ca5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                                        ),
+                                        SizedBox(
+                                          width: 9,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              'Secrets',
+                                              style: GoogleFonts.montserrat(
+                                                  color: Colors.white,
+                                                  fontSize: 18),
+                                            ),
+                                            SizedBox(
+                                              height: 4,
+                                            ),
+                                            Text(
+                                              'Not a Hobby 2020',
+                                              style: GoogleFonts.montserrat(
+                                                  color: Colors.grey.shade200),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  )),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Positioned(
