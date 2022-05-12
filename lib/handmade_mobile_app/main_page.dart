5a0e2165d29996bb7d770ca03e010594b144707a
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-05-13 01:35:00 +0630

    create main page

---
+++ lib/handmade_mobile_app/main_page.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class HandMadeApp extends StatelessWidget {
+  const HandMadeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {'/': ((context) => HandMadeMainPage())},
+    );
+  }
+}
+
+class HandMadeMainPage extends StatefulWidget {
+  const HandMadeMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<HandMadeMainPage> createState() => _HandMadeMainPageState();
+}
+
+class _HandMadeMainPageState extends State<HandMadeMainPage> {
+  final _pageController = PageController(viewportFraction: 1);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2016/03/23/04/01/woman-1274056_960_720.jpg'),
+                  fit: BoxFit.cover,
+                )),
+              )),
+          Positioned(
+              left: 16,
+              right: 16,
+              top: 36,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.add_shopping_cart,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              )),
+          Positioned(
+              left: 16,
+              bottom: MediaQuery.of(context).size.height / 2.2,
+              right: 16,
+              top: MediaQuery.of(context).size.height / 1.9,
+              child: Container(
+                child: Center(
+                  child: SmoothPageIndicator(
+                    controller: _pageController,
+                    count: 4,
+                    effect: WormEffect(activeDotColor: Colors.pinkAccent),
+                  ),
+                ),
+              )),
+          Positioned(
+              left: 16,
+              bottom: 16,
+              right: 16,
+              top: MediaQuery.of(context).size.height / 1.75,
+              child: PageView(
+                controller: _pageController,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: 24, right: 24, top: 32, bottom: 24),
+                    decoration:
+                        BoxDecoration(color: Colors.white.withOpacity(0.7)),
+                    child: Column(
+                      children: [
+                        Text(
+                          'METAMORPHOSE',
+                          style: GoogleFonts.openSans(
+                              fontSize: 16, letterSpacing: 1.2),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          'DRAGONFLIES WINGS',
+                          style: GoogleFonts.merriweather(
+                              fontSize: 17, fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(
+                          height: 21,
+                        ),
+                        Text(
+                          'Headpiece with wings of dragonflies very elegantly '
+                          'complement to your look at special events, parties '
+                          'weddings or just can bring to your everyday cloths more '
+                          'orginality',
+                          style: TextStyle(fontSize: 8, letterSpacing: 2),
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        SizedBox(
+                          height: 38,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    '\$55',
+                                    style: TextStyle(),
+                                  )),
+                              Expanded(flex: 8, child: Placeholder())
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.red),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.orange),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.purple),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:dribbble_flutter_ui4/fitness_activity_tracker_app/fitness_activity_tracker.dart';
+import 'package:dribbble_flutter_ui4/handmade_mobile_app/main_page.dart';
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(const FitnessActivityTracker());
+void main() => runApp(const HandMadeApp());
