5665b0c4066fd1b281960297adff741739ca3fb7
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-05-15 00:56:57 +0630

    create home page

--- lib/handmade_mobile_app/main_page.dart
+++ lib/handmade_mobile_app/main_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+const Color primaryColor = Color(0xffBc6e6b);
+const Color secondaryColor = Color(0xffC5bcbc);
+
 class HandMadeApp extends StatelessWidget {
   const HandMadeApp({Key? key}) : super(key: key);
 
@@ -10,7 +13,10 @@ class HandMadeApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {'/': ((context) => HandMadeMainPage())},
+      routes: {
+        '/': (context) => HandMadeHomePage(),
+        '/home': (context) => HandMadeMainPage()
+      },
     );
   }
 }
@@ -71,7 +77,7 @@ class _HandMadeMainPageState extends State<HandMadeMainPage> {
                   child: SmoothPageIndicator(
                     controller: _pageController,
                     count: 4,
-                    effect: WormEffect(activeDotColor: Colors.pinkAccent),
+                    effect: WormEffect(activeDotColor: Color(0xffC5BCBC)),
                   ),
                 ),
               )),
@@ -93,7 +99,9 @@ class _HandMadeMainPageState extends State<HandMadeMainPage> {
                         Text(
                           'METAMORPHOSE',
                           style: GoogleFonts.openSans(
-                              fontSize: 16, letterSpacing: 1.2),
+                              color: Color(0xff8c6e6b),
+                              fontSize: 16,
+                              letterSpacing: 1.2),
                         ),
                         SizedBox(
                           height: 16,
@@ -126,7 +134,22 @@ class _HandMadeMainPageState extends State<HandMadeMainPage> {
                                     '\$55',
                                     style: TextStyle(),
                                   )),
-                              Expanded(flex: 8, child: Placeholder())
+                              Expanded(
+                                  flex: 8,
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(16)),
+                                          color: Color(0xff8c6e6b)),
+                                      child: Center(
+                                        child: Text(
+                                          'see details',
+                                          style: GoogleFonts.merriweather(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w300,
+                                              color: Colors.white),
+                                        ),
+                                      )))
                             ],
                           ),
                         )
@@ -143,6 +166,156 @@ class _HandMadeMainPageState extends State<HandMadeMainPage> {
                     decoration: BoxDecoration(color: Colors.purple),
                   )
                 ],
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class HandMadeHomePage extends StatefulWidget {
+  const HandMadeHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HandMadeHomePage> createState() => _HandMadeHomePageState();
+}
+
+class _HandMadeHomePageState extends State<HandMadeHomePage> {
+  final PageController _pageController = PageController(viewportFraction: 0.9);
+  List<Widget> _buildBackground() {
+    return [
+      Positioned(
+          left: 0,
+          right: 0,
+          top: 0,
+          child: Container(
+            height: MediaQuery.of(context).size.height / 2.5,
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://cdn.pixabay.com/photo/2017/09/21/07/47/girl-2771001_960_720.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter:
+                        ColorFilter.mode(Colors.grey, BlendMode.color))),
+          )),
+      Positioned(
+          left: 0,
+          right: 0,
+          top: MediaQuery.of(context).size.height / 2.5,
+          child: Container(
+            height: MediaQuery.of(context).size.height / 3.4,
+            color: Colors.white,
+          )),
+      Positioned(
+          left: 0,
+          right: 0,
+          top: MediaQuery.of(context).size.height / 2.5 * 1.75,
+          child: Container(
+            height: MediaQuery.of(context).size.height / 2.5,
+            color: Color(0xffc5bcbc),
+          )),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          ..._buildBackground(),
+          Positioned(
+              left: 24,
+              right: 0,
+              top: MediaQuery.of(context).size.height / 3,
+              child: Container(
+                height: 100,
+                child: PageView(
+                  controller: _pageController,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(
+                          left: 16, top: 24, bottom: 24, right: 16),
+                      margin: EdgeInsets.only(right: 24),
+                      decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(24),
+                            bottomLeft: Radius.circular(24),
+                          )),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'METAMORPHOSE',
+                            style: GoogleFonts.openSans(
+                                color: Colors.white, letterSpacing: 2),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'BIRD BROOCH',
+                                style: GoogleFonts.merriweather(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    letterSpacing: 4),
+                              ),
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 24),
+                      decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(24),
+                              bottomLeft: Radius.circular(24))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 24),
+                      decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(24),
+                              bottomLeft: Radius.circular(24))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 24),
+                      decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(24),
+                              bottomLeft: Radius.circular(24))),
+                    )
+                  ],
+                ),
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              top: MediaQuery.of(context).size.height / 2.1,
+              child: Container(
+                height: 32,
+                child: Center(
+                  child: SmoothPageIndicator(
+                    controller: _pageController,
+                    count: 4,
+                    effect: WormEffect(
+                        dotWidth: 10,
+                        dotHeight: 10,
+                        activeDotColor: primaryColor,
+                        dotColor: secondaryColor),
+                  ),
+                ),
               ))
         ],
       ),
