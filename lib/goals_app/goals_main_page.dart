a8fcc006579c04a23f2dd7de8caa1e5cb88fc627
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-05-19 22:58:40 +0630

    make circle avatar

--- lib/goals_app/goals_main_page.dart
+++ lib/goals_app/goals_main_page.dart
@@ -193,6 +193,23 @@ class _MainPageState extends State<MainPage> {
                                       borderRadius: BorderRadius.only(
                                           bottomLeft: Radius.circular(16),
                                           bottomRight: Radius.circular(16))),
+                                  child: Row(
+                                    children: [
+                                      Flexible(
+                                          fit: FlexFit.tight,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.blueAccent,
+                                                borderRadius: BorderRadius.only(
+                                                  bottomLeft:
+                                                      Radius.circular(16),
+                                                )),
+                                          )),
+                                      Flexible(
+                                          fit: FlexFit.tight,
+                                          child: Container())
+                                    ],
+                                  ),
                                 ),
                               )
                             ],
@@ -201,13 +218,300 @@ class _MainPageState extends State<MainPage> {
                         Container(
                           width: 200,
                           margin: EdgeInsets.only(right: 16),
+                          padding: EdgeInsets.only(top: 16),
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(16),
-                              color: Colors.redAccent.withOpacity(0.4)),
+                              color: Colors.red.withOpacity(0.7)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16),
+                                child: Container(
+                                  height: 38,
+                                  width: 38,
+                                  decoration: BoxDecoration(
+                                      color: Colors.indigo,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                          left: -8,
+                                          top: -16,
+                                          child: Container(
+                                            height: 38,
+                                            width: 38,
+                                            decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                color: Colors.white
+                                                    .withOpacity(0.8)),
+                                          )),
+                                      Positioned(
+                                          top: -12,
+                                          child: Container(
+                                            height: 38,
+                                            width: 38,
+                                            decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                color: Colors.white
+                                                    .withOpacity(0.5)),
+                                          ))
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16),
+                                child: Text(
+                                  'Learn Flutter',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 16),
+                                child: Text(
+                                  'Flutte is Awesome!',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      letterSpacing: 1.2,
+                                      color: Colors.white.withOpacity(0.7)),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 16,
+                                  right: 8,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Progress',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    Text(
+                                      '10%',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 0),
+                                child: Container(
+                                  height: 12,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                      color: Colors.deepOrange[50],
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(16),
+                                          bottomRight: Radius.circular(16))),
+                                  child: Row(
+                                    children: [
+                                      Flexible(
+                                          fit: FlexFit.tight,
+                                          flex: 1,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.deepOrange[400],
+                                                borderRadius: BorderRadius.only(
+                                                    bottomLeft:
+                                                        Radius.circular(16))),
+                                          )),
+                                      Flexible(
+                                          flex: 9,
+                                          fit: FlexFit.tight,
+                                          child: Container())
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         )
                       ],
                     ),
-                  )
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 260,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                children: [
+                  Flexible(
+                      flex: 2,
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 38,
+                            width: 38,
+                            decoration: BoxDecoration(
+                                color: Colors.indigoAccent,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Center(
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Quick tasks for today',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      )),
+                  Flexible(
+                      flex: 8,
+                      child: ListView(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        children: [
+                          Container(
+                            height: 58,
+                            width: double.infinity,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 3),
+                                  child: Align(
+                                    alignment: Alignment.topCenter,
+                                    child: Container(
+                                      height: 12,
+                                      width: 12,
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.deepOrange),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Daily Meeting'),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      'Zoom, 10:00 AM',
+                                      style: TextStyle(color: Colors.grey[500]),
+                                    )
+                                  ],
+                                ),
+                                Spacer(),
+                                CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 58,
+                            width: double.infinity,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 3),
+                                  child: Align(
+                                    alignment: Alignment.topCenter,
+                                    child: Container(
+                                      height: 12,
+                                      width: 12,
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.deepOrange),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Designer\'s Interview'),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      'Green Meeting Room, 3:00 PM',
+                                      style: TextStyle(color: Colors.grey[500]),
+                                    )
+                                  ],
+                                ),
+                                Spacer(),
+                                CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://images.pexels.com/photos/1468379/pexels-photo-1468379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 58,
+                            width: double.infinity,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 3),
+                                  child: Align(
+                                    alignment: Alignment.topCenter,
+                                    child: Container(
+                                      height: 12,
+                                      width: 12,
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.greenAccent),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Daily Sketching'),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      'Open Space, 05:00 PM',
+                                      style: TextStyle(color: Colors.grey[500]),
+                                    )
+                                  ],
+                                ),
+                                Spacer(),
+                              ],
+                            ),
+                          )
+                        ],
+                      ))
                 ],
               ),
             )
