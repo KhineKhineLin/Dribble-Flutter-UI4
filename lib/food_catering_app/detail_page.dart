64332fbd5c1e22dc6198c8e3e1e599ccc0b4bbc4
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-06-12 13:50:14 +0630

    create detail page

---
+++ lib/food_catering_app/detail_page.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+              child: Hero(
+                tag: 'food_01',
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://cdn.pixabay.com/photo/2016/12/06/18/27/tomatoes-1887240_960_720.jpg'),
+                          fit: BoxFit.cover)),
+                ),
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+                height: MediaQuery.of(context).size.height / 2 - 60,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(32),
+                        topLeft: Radius.circular(32))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Cezarre Original',
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 120.0, top: 16),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 16,
+                          ),
+                          Text(
+                            '4.9',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.timer,
+                            color: Colors.grey,
+                            size: 16,
+                          ),
+                          Text(
+                            '15 mins',
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.info_outline,
+                            color: Colors.grey,
+                            size: 16,
+                          ),
+                          Text(
+                            'Cheese',
+                            style: TextStyle(fontSize: 16, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 32.0, top: 8, bottom: 8),
+                      child: Text(
+                        'cheesy mayo sauce and mozzrella, tomatoes, green pepper, onion'
+                        'cheesy mayo sauce and mozzrella, tomatoes, green pepper, onion'
+                        'cheesy mayo sauce and mozzrella, tomatoes, green pepper, onion',
+                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                      ),
+                    ),
+                    Container(
+                      height: 31,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.monetization_on,
+                            color: Colors.grey,
+                            size: 24,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            '123.00',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: VerticalDivider(
+                              width: 24,
+                              thickness: 2,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            '230g',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Spacer(),
+                          Container(
+                            width: 120,
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(6)),
+                            child: Center(
+                              child: Text(
+                                'Add to cart',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      height: 30,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.monetization_on,
+                            color: Colors.grey,
+                            size: 24,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            '173.00',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: VerticalDivider(
+                              width: 24,
+                              thickness: 2,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            '321g',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Spacer(),
+                          Container(
+                            width: 120,
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(6)),
+                            child: Center(
+                              child: Text(
+                                'Add to cart',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/food_catering_app/home_page.dart
+++ lib/food_catering_app/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'detail_page.dart';
+
 class FoodCateringHomePage extends StatefulWidget {
   const FoodCateringHomePage({Key? key}) : super(key: key);
 
@@ -21,131 +23,139 @@ class _FoodCateringHomePageState extends State<FoodCateringHomePage> {
               decoration: BoxDecoration(color: Colors.blueGrey[50]),
               child: ListView(
                 children: [
-                  Container(
-                    height: 120,
-                    margin: EdgeInsets.only(bottom: 16, left: 16, right: 16),
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.1),
-                              blurRadius: 3,
-                              spreadRadius: 5)
-                        ],
-                        borderRadius: BorderRadius.circular(8),
-                        color: Colors.white),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            flex: 4,
+                  InkWell(
+                    onTap: () => Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => DetailPage())),
+                    child: Container(
+                      height: 120,
+                      margin: EdgeInsets.only(bottom: 16, left: 16, right: 16),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 3,
+                                spreadRadius: 5)
+                          ],
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.white),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 4,
+                              child: Hero(
+                                tag: 'food_01',
+                                child: Container(
+                                  margin: EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(8),
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              'https://cdn.pixabay.com/photo/2016/12/06/18/27/tomatoes-1887240_960_720.jpg'),
+                                          fit: BoxFit.cover)),
+                                ),
+                              )),
+                          Expanded(
+                            flex: 8,
                             child: Container(
-                              margin: EdgeInsets.all(8),
-                              decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(8),
-                                  image: DecorationImage(
-                                      image: NetworkImage(
-                                          'https://cdn.pixabay.com/photo/2016/12/06/18/27/tomatoes-1887240_960_720.jpg'),
-                                      fit: BoxFit.cover)),
-                            )),
-                        Expanded(
-                          flex: 8,
-                          child: Container(
-                            padding: EdgeInsets.only(bottom: 8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Cezarre Original',
-                                  style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 48),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.star,
-                                        color: Colors.yellow,
-                                        size: 14,
-                                      ),
-                                      Text(
-                                        '4.9',
-                                        style: TextStyle(fontSize: 12),
-                                      ),
-                                      Spacer(),
-                                      Icon(
-                                        Icons.timer,
-                                        color: Colors.grey,
-                                        size: 14,
-                                      ),
-                                      Text('15 mins',
-                                          style: TextStyle(fontSize: 12)),
-                                      Spacer(),
-                                      Icon(
-                                        Icons.info_outline,
-                                        color: Colors.grey,
-                                        size: 14,
-                                      ),
-                                      Text(
-                                        'Cheese',
-                                        style: TextStyle(fontSize: 12),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 32.0),
-                                  child: Text(
-                                    'cheesy mayo sauce and mozzarella, tomatoes, green pepper, onion,',
+                              padding: EdgeInsets.only(bottom: 8),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Cezarre Original',
                                     style: TextStyle(
-                                        fontSize: 10, color: Colors.grey),
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 24.0),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Icon(
-                                        Icons.monetization_on,
-                                        color: Colors.grey,
-                                        size: 16,
-                                      ),
-                                      SizedBox(
-                                        width: 8,
-                                      ),
-                                      Text(
-                                        '123.00',
-                                        style: TextStyle(fontSize: 16),
-                                      ),
-                                      Spacer(),
-                                      Container(
-                                        width: 82,
-                                        height: 24,
-                                        decoration: BoxDecoration(
-                                            color: Colors.red,
-                                            borderRadius:
-                                                BorderRadius.circular(5)),
-                                        child: Center(
-                                          child: Text(
-                                            'Add to cart',
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10),
-                                          ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 48),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                          size: 14,
                                         ),
-                                      )
-                                    ],
+                                        Text(
+                                          '4.9',
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                        Spacer(),
+                                        Icon(
+                                          Icons.timer,
+                                          color: Colors.grey,
+                                          size: 14,
+                                        ),
+                                        Text('15 mins',
+                                            style: TextStyle(fontSize: 12)),
+                                        Spacer(),
+                                        Icon(
+                                          Icons.info_outline,
+                                          color: Colors.grey,
+                                          size: 14,
+                                        ),
+                                        Text(
+                                          'Cheese',
+                                          style: TextStyle(fontSize: 12),
+                                        )
+                                      ],
+                                    ),
                                   ),
-                                )
-                              ],
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 32.0),
+                                    child: Text(
+                                      'cheesy mayo sauce and mozzarella, tomatoes, green pepper, onion,',
+                                      style: TextStyle(
+                                          fontSize: 10, color: Colors.grey),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 24.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Icon(
+                                          Icons.monetization_on,
+                                          color: Colors.grey,
+                                          size: 16,
+                                        ),
+                                        SizedBox(
+                                          width: 8,
+                                        ),
+                                        Text(
+                                          '123.00',
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                        Spacer(),
+                                        Container(
+                                          width: 82,
+                                          height: 24,
+                                          decoration: BoxDecoration(
+                                              color: Colors.red,
+                                              borderRadius:
+                                                  BorderRadius.circular(5)),
+                                          child: Center(
+                                            child: Text(
+                                              'Add to cart',
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                   Container(
