c568cd67963b2a0dd9f912f884d68ae414db6a12
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-06-12 23:58:24 +0630

    create favourite icon buttons

--- lib/food_delivery_app/food_delivery_main_page.dart
+++ lib/food_delivery_app/food_delivery_main_page.dart
@@ -403,14 +403,165 @@ class _FoodDeliveryMainPageState extends State<FoodDeliveryMainPage> {
             Container(
               height: 460,
               child: ListView.builder(
+                  padding: EdgeInsets.zero,
                   shrinkWrap: true,
                   scrollDirection: Axis.vertical,
-                  itemCount: 10,
+                  itemCount: shops.length,
                   itemBuilder: (context, index) {
                     return Container(
-                      height: 120,
+                      height: 195,
                       margin: EdgeInsets.all(8),
-                      child: Placeholder(),
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          border: Border.all(color: Colors.grey)),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 4,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image:
+                                            NetworkImage(shops[index].imgPath),
+                                        fit: BoxFit.cover)),
+                              )),
+                          Expanded(
+                              flex: 8,
+                              child: Container(
+                                margin: EdgeInsets.only(left: 8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+                                            flex: 8,
+                                            child: Text(
+                                              shops[index].title,
+                                              style: TextStyle(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                        Expanded(
+                                            flex: 2,
+                                            child: shops[index].isLike
+                                                ? IconButton(
+                                                    icon: Icon(Icons.favorite),
+                                                    color: Colors.red,
+                                                    onPressed: () {
+                                                      setState(() {
+                                                        shops[index].isLike =
+                                                            !shops[index]
+                                                                .isLike;
+                                                      });
+                                                    },
+                                                  )
+                                                : IconButton(
+                                                    icon: Icon(
+                                                        Icons.favorite_border),
+                                                    color: Colors.red,
+                                                    onPressed: () {
+                                                      setState(() {
+                                                        shops[index].isLike =
+                                                            !shops[index]
+                                                                .isLike;
+                                                      });
+                                                    },
+                                                  ))
+                                      ],
+                                    ),
+                                    Spacer(),
+                                    Text(
+                                      shops[index].food,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.black.withOpacity(0.6)),
+                                    ),
+                                    Text(
+                                      shops[index].price,
+                                      style: TextStyle(
+                                          fontSize: 10, color: Colors.grey),
+                                    ),
+                                    Spacer(),
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                            flex: 1,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Icon(
+                                                      Icons.star,
+                                                      color: Colors.yellow,
+                                                      size: 20,
+                                                    ),
+                                                    Icon(
+                                                      Icons.star,
+                                                      color: Colors.yellow,
+                                                      size: 20,
+                                                    ),
+                                                    Icon(
+                                                      Icons.star,
+                                                      color: Colors.yellow,
+                                                      size: 20,
+                                                    ),
+                                                    Icon(
+                                                      Icons.star,
+                                                      color: Colors.yellow,
+                                                      size: 20,
+                                                    ),
+                                                    Icon(
+                                                      Icons.star,
+                                                      color: Colors.grey,
+                                                      size: 19,
+                                                    )
+                                                  ],
+                                                ),
+                                                Text(
+                                                  shops[index].price,
+                                                  style: TextStyle(
+                                                      fontSize: 10,
+                                                      color: Colors.grey),
+                                                )
+                                              ],
+                                            )),
+                                        Expanded(
+                                            flex: 1,
+                                            child: shops[index].freeDelivery
+                                                ? Container(
+                                                    margin: EdgeInsets.only(
+                                                        left: 32),
+                                                    height: 16,
+                                                    decoration: BoxDecoration(
+                                                      color: Colors.orange[300],
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              4),
+                                                    ),
+                                                    child: Center(
+                                                      child: Text(
+                                                        'free delivery',
+                                                        style: TextStyle(
+                                                            color: Colors.white,
+                                                            fontSize: 10),
+                                                      ),
+                                                    ),
+                                                  )
+                                                : Container())
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ))
+                        ],
+                      ),
                     );
                   }),
             )
@@ -420,3 +571,51 @@ class _FoodDeliveryMainPageState extends State<FoodDeliveryMainPage> {
     );
   }
 }
+
+class Shop {
+  String imgPath;
+  String title;
+  String food;
+  String price;
+  int reviewCount;
+  bool isLike;
+  bool freeDelivery;
+  Shop(
+      {required this.imgPath,
+      required this.title,
+      required this.food,
+      required this.price,
+      required this.reviewCount,
+      required this.isLike,
+      required this.freeDelivery});
+}
+
+List<Shop> shops = [
+  Shop(
+      imgPath:
+          'https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg',
+      title: 'Paco fish taco and more',
+      food: 'Seafood, Mexican, fish tacos',
+      price: 'min, order \$15.00',
+      reviewCount: 156,
+      isLike: true,
+      freeDelivery: false),
+  Shop(
+      imgPath:
+          'https://cdn.pixabay.com/photo/2017/12/05/20/08/pizza-3000271_960_720.jpg',
+      title: 'The Kitchen',
+      food: 'Pizza, burgers, fries',
+      price: 'min, order \$10.00',
+      reviewCount: 123,
+      isLike: false,
+      freeDelivery: true),
+  Shop(
+      imgPath:
+          'https://cdn.pixabay.com/photo/2016/03/05/20/06/abstract-1238656_960_720.jpg',
+      title: 'Joe and sons grill steak house',
+      food: 'American, steak, ribs',
+      price: 'min, order \$10.00',
+      reviewCount: 98,
+      isLike: false,
+      freeDelivery: true)
+];
