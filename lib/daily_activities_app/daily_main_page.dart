c7b15908a60b66ed829188c2502c399013e7d1e6
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-06-25 18:50:39 +0630

    insert text spans

--- lib/daily_activities_app/daily_main_page.dart
+++ lib/daily_activities_app/daily_main_page.dart
@@ -45,13 +45,90 @@ class DailyMainPage extends StatelessWidget {
                           ],
                         ),
                       )),
-                      Expanded(child: Placeholder())
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Card(
+                          color: Theme.of(context).cardColor,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  flex: 1,
+                                  child: Icon(
+                                    Icons.thumb_up_outlined,
+                                    color: Colors.orange,
+                                    size: 42,
+                                  )),
+                              Expanded(
+                                  flex: 2,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'High-five!',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        'you can finish over 80% of your activities each day.',
+                                        style: TextStyle(fontSize: 12),
+                                      )
+                                    ],
+                                  )),
+                              Expanded(
+                                  flex: 1,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: FloatingActionButton(
+                                      onPressed: () {},
+                                      child: Text('ok'),
+                                      backgroundColor: Colors.orange,
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                      ))
                     ],
                   ),
                 ),
                 Container(
-                  height: MediaQuery.of(context).size.height / 3,
-                  child: Placeholder(),
+                  height: MediaQuery.of(context).size.height / 3.4,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Completion\nTracker',
+                                  style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                Text.rich(TextSpan(children: [
+                                  TextSpan(text: 'You completed '),
+                                  TextSpan(text: '2'),
+                                  TextSpan(text: 'of'),
+                                  TextSpan(text: '4\n'),
+                                  TextSpan(text: 'activities so far!')
+                                ]))
+                              ],
+                            ),
+                          )),
+                      Expanded(flex: 1, child: Placeholder())
+                    ],
+                  ),
                 ),
                 Row(
                   children: [
