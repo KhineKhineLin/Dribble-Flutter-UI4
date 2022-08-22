e2edb4160eb5ba9fbaf933672a770f1a0a1ac4cd
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2022-08-22 11:11:55 +0630

    change Container to AnimatedContainer

--- lib/marvie_ios_app/marvie_main_page.dart
+++ lib/marvie_ios_app/marvie_main_page.dart
@@ -20,10 +20,26 @@ class MarvieHome extends StatefulWidget {
 }
 
 class _MarvieHomeState extends State<MarvieHome> {
-  double viewScale=1.0;
-  double viewLeft=0.0;
-  double viewRight=0.0;
-  double viewTop=0.0;
+  double viewScale = 1.0;
+  double viewLeft = 0.0;
+  double viewRight = 0.0;
+  double viewTop = 0.0;
+  double viewAngel = 0.0;
+
+  double view2Scale = 1.0;
+  double view2Left = 0.0;
+  double view2Right = 0.0;
+  double view2Top = 0.0;
+  double view2Angel = 0.0;
+
+  double view3Scale = 1.0;
+  double view3Left = 0.0;
+  double view3Right = 0.0;
+  double view3Top = 0.0;
+  double view3Angel = 0.0;
+
+  double circles = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +54,9 @@ class _MarvieHomeState extends State<MarvieHome> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SizedBox(height: 34,),
+                    const SizedBox(
+                      height: 34,
+                    ),
                     Container(
                       height: 84,
                       width: 84,
@@ -59,10 +77,11 @@ class _MarvieHomeState extends State<MarvieHome> {
                     ),
                     const Padding(
                       padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Text('Khinekhinel',style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18
-                      ),),
+                      child: Text(
+                        'Khinekhinel',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical: 16.0),
@@ -124,56 +143,183 @@ class _MarvieHomeState extends State<MarvieHome> {
                         ],
                       ),
                     ),
-                    const SizedBox(height: 34 ,),
-                    Container(
-                      width: 140,
-                      padding: const EdgeInsets.all(16),
-                      decoration:  BoxDecoration(
-                        color: Colors.greenAccent,
-                        borderRadius: BorderRadius.circular(12)
-                      ),
-                      child: const Center(
-                        child: Text('Next',style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
-                        ),),
+                    const SizedBox(
+                      height: 34,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          viewScale = 1.0;
+                          viewLeft = 0.0;
+                          viewRight = 0.0;
+                          viewTop = 0.0;
+                          viewAngel = 0.0;
+
+                          view2Scale = 1.0;
+                          view2Left = 0.0;
+                          view2Right = 0.0;
+                          view2Top = 0.0;
+                          view2Angel = 0.0;
+
+                          view3Scale = 1.0;
+                          view3Left = 0.0;
+                          view3Right = 0.0;
+                          view3Top = 0.0;
+                          view3Angel = 0.0;
+
+                          circles = 0.0;
+                        });
+                      },
+                      child: Container(
+                        width: 140,
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                            color: Colors.greenAccent,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Center(
+                          child: Text(
+                            'Next',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
                       ),
                     )
                   ],
                 ),
               )),
           Positioned(
-            top: viewTop,
-            left: viewLeft,
-            bottom: 0,
-            right: viewRight,
-            child: Transform.rotate(
-              angle: -0.2,
-              child: Transform.scale(
-                scale: viewScale,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red[400],
-                    borderRadius: BorderRadius.circular(24),
-                    
-                  ),
-                child: Center(
-                  child: MaterialButton(
-                    onPressed: (){
-                      setState(() {
-                        viewScale=0.8;
-                        viewRight=-160;
-                        viewTop=120;
-                        viewLeft=MediaQuery.of(context).size.width/2;
-                      });
-                    },
-                    color: Colors.tealAccent,
-                    child: const Text('Show Menu'),
+              top: viewTop,
+              left: viewLeft,
+              bottom: -210,
+              right: viewRight,
+              child: Transform.rotate(
+                angle: viewAngel,
+                child: Transform.scale(
+                  scale: viewScale,
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    decoration: BoxDecoration(
+                      color: Colors.red[400],
+                      borderRadius: BorderRadius.circular(circles),
+                    ),
+                    child: Center(
+                      child: MaterialButton(
+                        onPressed: () {
+                          setState(() {
+                            viewScale = 0.8;
+                            viewRight = MediaQuery.of(context).size.width / 2;
+                            viewTop = 120;
+                            viewLeft = MediaQuery.of(context).size.width / 2;
+                            viewAngel = 0.2;
+                          });
+                        },
+                        color: Colors.tealAccent,
+                        child: const Text('Show Menu'),
+                      ),
+                    ),
                   ),
                 ),
+              )),
+          Positioned(
+              top: view2Top,
+              left: view2Left,
+              bottom: -210,
+              right: view2Right,
+              child: Transform.rotate(
+                  angle: view2Angel,
+                  child: Transform.scale(
+                      scale: view2Scale,
+                      child: AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        decoration: BoxDecoration(
+                            color: Colors.tealAccent[400],
+                            borderRadius: BorderRadius.circular(circles)),
+                        child: Center(
+                          child: MaterialButton(
+                            onPressed: () {
+                              setState(() {
+                                viewScale = 0.8;
+                                viewRight =
+                                    -MediaQuery.of(context).size.width / 1.8;
+                                viewTop = 84;
+                                viewLeft =
+                                    MediaQuery.of(context).size.width / 1.5;
+                                viewAngel = -0.3;
+
+                                view2Scale = 0.8;
+                                view2Right =
+                                    -MediaQuery.of(context).size.width / 1.4;
+                                view2Top = 24;
+                                view2Left =
+                                    MediaQuery.of(context).size.width / 1.3;
+                                view2Angel = -0.2;
+
+                                view3Scale = 0.8;
+                                view3Right =
+                                    -MediaQuery.of(context).size.width / 1.5;
+                                view3Top = 0;
+                                view3Left =
+                                    MediaQuery.of(context).size.width / 1.1;
+                                view3Angel = -0.1;
+                              });
+                            },
+                            color: Colors.tealAccent,
+                            child: const Text('Show Menu'),
+                          ),
                         ),
-              ),
-            ))
+                      )))),
+          Positioned(
+              top: view3Top,
+              left: view3Left,
+              bottom: -210,
+              right: view3Right,
+              child: Transform.rotate(
+                  angle: view3Angel,
+                  child: Transform.scale(
+                      scale: view3Scale,
+                      child: AnimatedContainer(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.circular(circles),
+                        ),
+                        duration: Duration(seconds: 1),
+                        child: Center(
+                          child: MaterialButton(
+                            onPressed: () {
+                              setState(() {
+                                viewScale = 0.8;
+                                viewRight =
+                                    -MediaQuery.of(context).size.width / 1.8;
+                                viewTop = 84;
+                                viewLeft =
+                                    MediaQuery.of(context).size.width / 1.4;
+                                viewAngel = -0.3;
+
+                                view2Scale = 0.8;
+                                view2Right =
+                                    -MediaQuery.of(context).size.width / 1.4;
+                                view2Top = 24;
+                                view2Left =
+                                    MediaQuery.of(context).size.width / 1.3;
+                                view2Angel = -0.2;
+
+                                view3Scale = 0.8;
+                                view3Right =
+                                    -MediaQuery.of(context).size.width / 1.5;
+                                view3Top = 0;
+                                view3Left =
+                                    MediaQuery.of(context).size.width / 1.2;
+                                view3Angel = -0.1;
+                                circles = 48.0;
+                              });
+                            },
+                            color: Colors.tealAccent,
+                            child: const Text('Show Menu'),
+                          ),
+                        ),
+                      ))))
         ],
       ),
     );
